d01a9a13da3ceac144f2960a6eeb4893993f978c
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-19 08:49:34 -0500

    Update Governments.js

--- Governments.js
+++ Governments.js
@@ -1,1 +1,38 @@
  let my_butt = document.getElementsById("sectionTabs");
+// Create a new element
+const newElement = document.createElement("div");
+newElement.textContent = "This is a new paragraph"; 
+
+// Append the new element to the div
+my.appendChild(newElement);
+
+G.AddData({
+name:'GovernmentMod',
+author:'GothicHamster',
+desc:'A mod that adds governments.',
+engineVersion:1,
+manifest:0,
+requires:['Default dataset*'],
+sheets:{'spicySheet':'img/spicyModIconSheet.png'},
+func:function()
+{
+		
+	//Policies
+	
+	new G.Policy({
+		name:'eat herbs',
+		desc:'[herb]s are eaten, which may be unhealthy.',
+		icon:[6,12,4,6],
+		cost:{'influence':1},
+		startMode:'on',
+		req:{'rules of food':true},
+		effects:[
+			{type:'make part of',what:['herb'],parent:'food'},
+		],
+		effectsOff:[
+			{type:'make part of',what:['herb'],parent:''},
+		],
+		category:'food',
+	});
+}
+});
