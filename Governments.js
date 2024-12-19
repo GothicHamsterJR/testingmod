91c913dbbfc0984e77949f64534606520b9144f0
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-19 06:28:55 -0500

    Update Governments.js

--- Governments.js
+++ Governments.js
@@ -11,7 +11,7 @@ func:function()
 	new G.Policy({
 		name:'eat herbs',
 		desc:'[herb]s are eaten, which may be unhealthy.',
-		icon:[4,12,5,7],
+		icon:[6,4,5,7],
 		cost:{'influence':1},
 		startMode:'on',
 		req:{'rules of food':false},
