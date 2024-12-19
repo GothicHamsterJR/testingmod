0d898c848cd3715b2f8d1b304f52b4dd1a9b22ae
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-19 06:33:40 -0500

    Update Governments.js

--- Governments.js
+++ Governments.js
@@ -8,13 +8,26 @@ requires:['Default dataset*'],
 sheets:{'spicySheet':'img/spicyModIconSheet.png'},
 func:function()
 {
+	
+	//Categories
+	
+	G.governmentCategories.push(
+		{id:'test1',name:'test1'},
+		{id:'test2',name:'test2'},
+		{id:'test3',name:'test4'},
+		{id:'test5',name:'test5'},
+		{id:'test6',name:'test6'}
+	);
+
+	//Policies
+	
 	new G.Policy({
 		name:'eat herbs',
 		desc:'[herb]s are eaten, which may be unhealthy.',
 		icon:[6,12,4,6],
 		cost:{'influence':1},
 		startMode:'on',
-		req:{'rules of food':false},
+		req:{'rules of food':true},
 		effects:[
 			{type:'make part of',what:['herb'],parent:'food'},
 		],
