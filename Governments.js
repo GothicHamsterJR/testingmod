0a2f66594e491707be7326d097c9b4703dab753b
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-20 06:49:04 -0500

    Update Governments.js

--- Governments.js
+++ Governments.js
@@ -1,3 +1,33 @@
+G.AddData({
+name:'GovernmentMod',
+author:'GothicHamster',
+desc:'A mod that adds governments.',
+engineVersion:1,
+manifest:0,
+requires:['Default dataset*'],
+sheets:{'spicySheet':'img/spicyModIconSheet.png'},
+func:function()
+{
+		
+	//Policies
+	
+	new G.Policy({
+		name:'eat herbs',
+		desc:'[herb]s are eaten, which may be unhealthy.',
+		icon:[6,12,4,6],
+		cost:{'influence':1},
+		startMode:'on',
+		req:{'rules of food':true},
+		effects:[
+			{type:'make part of',what:['herb'],parent:'food'},
+		],
+		effectsOff:[
+			{type:'make part of',what:['herb'],parent:''},
+		],
+		category:'food',
+	});
+}
+//other mod startsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 G.hSetting=[];
 	G.hSettingByName=[];
 	G.hSettingCategory=[];
@@ -267,3 +297,4 @@ G.writeHSettingButton=function(obj)
 		'</div>';
 		return str;
 	}
+});
