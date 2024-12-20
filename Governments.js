9ee473a8cf27f9a4eec1c7d5fb27f6546fb80e6c
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-20 06:47:25 -0500

    Update Governments.js

--- Governments.js
+++ Governments.js
@@ -1,14 +1,269 @@
-{
+G.hSetting=[];
+	G.hSettingByName=[];
+	G.hSettingCategory=[];
+	G.HSettingsLoaded=false;
+
+	G.HSetting=function(obj)
+	{
+		this.type='policy';	// required to work with G.checkReq()
+		this.category='debug';	// best way to keep them hidden for now
+		this.family='';
+		this.startWith=0;
+		this.icon=[0,0];
+		this.modes=[];
+		this.mode=0;
+		this.req={};
+
+		this.cost={};
+		this.startsWith=true;
+		this.visible=false;
+
+		for (var i in obj) this[i]=obj[i];
+		this.id=G.policy.length;
+		if (!this.displayName) this.displayName=cap(this.name);
+		if(!this.hcategory) this.hcategory=G.context.name;
+		G.policy.push(this);
+		G.policyByName[this.name]=this;
+		G.setDict(this.name,this);
+		if (this.modes.length==0)
+		{
+			//no modes defined? auto-populate as simple on/off switch
+			this.modes['off']={name:'Disabled',desc:'This policy is disabled.'};
+			this.modes['on']={name:'Enabled',desc:'This policy is enabled.'};
+			if (this.effectsOff) this.modes['off'].effects=this.effectsOff;
+			if (this.effects) this.modes['on'].effects=this.effects;
+			this.binary=true;
+		}
+		this.mod=G.context;
+
+		// extra stuff here
+		G.hSetting.push(this);
+		G.hSettingByName[this.name]=this;
+	}
+
+// wrapper function to make sure all settings are displayed when added
+	G.addHSetting=function(obj)
+	{
+		new G.HSetting(obj);
+		if (G.HSettingsLoaded) G.buildTabs();	// after intial load, rebuild the tabs to display new settings
+	}
+
+	G.baseHSetting=function(obj)
+	{
+		this.type='setting';
+		for (var i in obj) this[i]=obj[i];
+		G.hSetting.push(this);
+		G.hSettingByName[this.name]=this;
+	}
+
+	G.getHSetting=function(name) {
+		if (!G.hSettingByName[name])
+			ERROR('No policy exists with the name '+name+'.');
+		else
+			return G.hSettingByName[name];
+	}
+
+	G.checkHSetting=function(name)
+	{
+		var me=G.getHSetting(name);
+//		if (!me.visible) return 0;
+		return me.mode.id;
+	}
+
+	G.setHSettingModeByName=function(name,mode)
+	{
+		me=G.getHSetting(name);
+		G.setHSettingMode(me,me.modes[mode]);
+	}
+
+	G.setHSettingMode=function(me,mode)
+	{
+		//free old mode uses, and assign new mode uses
+		var oldMode=me.mode;
+		var newMode=mode;
+		if (oldMode!=newMode)
+		{
+			me.mode=mode;
+
+			if (me.mode.effects) G.applyKnowEffects(me.mode,false,true);
+			
+			if (me.l != 0)	// only update visuals for displayed policy buttons
+			{
+				if (G.getSetting('animations')) triggerAnim(me.l,'plop');
+				if (me.binary)
+				{
+					if (mode.id=='off') me.l.classList.add('off');
+					else me.l.classList.remove('off');
+				}
+			}
+
+			// function callback specifically for HSettings
+			if (me.effects.onChange) me.effects.onChange.func();
+		}
+	}
+
+	// add and manage categories of settings for automation population
+	G.addHSettingCategory=function(obj)
+	{
+		if (!obj.id) obj.id=G.context.name;	// if not specified, add a category specific to the calling mod
+		if (!obj.name) obj.name=obj.id;
+
+		// make sure the category doesn't exist
+		if (G.hSettingCategory[obj.id]===undefined)
+		{
+			G.hSettingCategory[obj.id]=obj;
+		} else {
+			console.error("HSetting category ",obj.id," already exists");
+		}
+	}
+
+	G.updateHSettingCategory=function(obj)
+	{
+		// make sure the category exists
+		if (G.hSettingCategory['obj.id']!==undefined)
+		{
+			for (var i in obj)
+			{
+				G.hSettingCategory['obj.id'][i] = obj[i];
+			}
+		} else {
+			console.error('No such hSetting category: ',obj.id);
+		}
+	}
+// tabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+G.writeHSettingButton=function(obj)
+	{
+		G.pushCallback(function(obj){return function(){
+			var div=l('hsettingButton-'+obj.id);
+			if (div)
+			{
+				var me=G.getHSetting(obj.name);
+				if (me.binary==true)
+				{
+					var on = (G.checkHSetting(obj.name)=="on");
+
+					div.innerHTML=obj.text||me.name;
+					if (on) div.classList.add('on');
+				}
+
+				div.onclick=function(div,name,value,siblings){return function(){G.clickHSettingButton(div,name,value,siblings);}}(div,obj.name,obj.value,obj.siblings);
+				if (obj.tooltip) G.addTooltip(div,function(str){return function(){return str;};}(obj.tooltip),{offY:-8});
+			}
+		}}(obj));
+		return '<div class="button" id="hsettingButton-'+obj.id+'"></div>';
+	}
+
+	G.clickHSettingButton=function(div,name,value,siblings)
+	{
+		var me=G.getHSetting(name);
+
+		if (me.binary)
+		{
+			if (G.checkHSetting(name)=="on")
+			{
+				G.setHSettingMode(me,me.modes["off"]);
+			}
+			else{
+				G.setHSettingMode(me,me.modes["on"]);
+			}
+		}
+		else
+		{
+			G.setHSettingMode(me,me.modes[value]);
+		}
+
+		if (div)
+		{
+			var on=(me.mode.id=="on");
+			if (on) div.classList.add('on'); else div.classList.remove('on');
+			if (siblings)
+			{
+				for (var i in siblings)
+				{
+					if (('hsettingButton-'+siblings[i])!=div.id)
+					{l('hsettingButton-'+siblings[i]).classList.remove('on');}
+				}
+			}
+		}
+	}
+
+	// A function to write each category of settings and buttons
+	G.writeHSettingCategories=function()
+	{
+		var str='';
+		for (c in G.hSettingCategory)
+		{
+			if (c=='hidden') continue;
+			var category=G.hSettingCategory[c];
+			str+='<div class="barred fancyText">'+category.displayName+'</div>';
+			for (var i in G.hSetting)
+			{
+				var s = G.hSetting[i];
+				if (s.hcategory == c)
+				{
+					if (s.type=='setting')
+					{
+						str+=G.writeSettingButton({
+							id:s.id,
+							name:s.name,
+							text:s.displayName,
+							tooltip:s.desc
+						});
+					} else {
+						str+=G.writeHSettingButton({
+							id:s.name,
+							name:s.name,
+							text:s.displayName,
+							tooltip:s.desc,
+						});
+					}
+				}
+			}
+			str+='<br /><br />';
+		}
+		return str;
+	}
+
+	// only add the tab once per page load (otherwise tab will duplicate itself with new game or mod reloading)
+	for (t in G.tabs) {
+		if (G.tabs[t].name=='Heritage')
+		{
+			G.HSettingsLoaded = true;
+		}
+	}
+
+	if (!G.HSettingsLoaded)
+	{
 		G.tabs.push({
-			name:'Government',
-			id:'government',
+			name:'Heritage',
+			id:'heritage',
 			popup:true,
-			addClass:'left',
-			desc:'Set your Government.'
+			addClass:'right',
+			desc:'Options and information about the Heritage mod pack.'
 		});
+		// Don't make assumptions about the existing tabs
+		// (or another mod that does the same thing)
+		// make sure everything is numbered and built properly
 		for (var i=0;i<G.tabs.length;i++){G.tabs[i].I=i;}
 		G.buildTabs();
 		
 	}
 
-	G.tabPopup['government']=function()
+	G.tabPopup['heritage']=function()
+	{
+		var str='';
+		
+		// disclaimer blurb for the top
+		str+='<div class="par">'+
+		'<b>NeverEnding Heritage</b> is a modpack for NeverEnding Legacy by <a href="https://github.com/geekahedron/heritage" target="_blank">geekahedron</a>.'+
+		'It is currently in early alpha, may feature strange and exotic bugs, and may be updated at any time.</div>'+
+		'<div class="par">While in development, the modpack may be unstable and subject to changes, but the overall goal is to '+
+		'expand and improve the legacy with flexible, balanced, user-created content and improvements to existing mechanics.</div>'+
+		'<div class="fancyText title">Heritage Modpack</div>'+
+		G.writeHSettingCategories()+
+		'<div class="divider"></div>'+
+		'<div class="buttonBox">'+
+		G.dialogue.getCloseButton()+
+		'</div>';
+		return str;
+	}
