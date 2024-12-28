1ff21624dc0126bed5f20c0b8f49ddedc6811eb2
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2024-12-28 16:31:26 -0500

    Update mayanmod.js

--- mayanmod.js
+++ mayanmod.js
@@ -1,2 +1,13 @@
-
-<div id="tab-tech" class="tab on bgLight plop">Research</div>
+G.AddData({
+name:'Example mod',
+author:'Orteil',
+desc:'A simple example mod that adds hot peppers and hot sauce.',
+engineVersion:1,
+manifest:'modManifest.js',
+requires:['Default dataset*'],
+sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
+	<div id="tab-government" class="tab on bgLight plop">Governments</div>
+}
+});
