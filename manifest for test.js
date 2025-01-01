a878c4697474197561ecb8d128cc443bb0541042
GothicHamsterJR <g0thic.hamst3r@gmail.com>
2025-01-01 09:12:27 -0500

    Update and rename manifest for test.js to manifest for test.css

--- manifest for test.js
+++
@@ -1,7 +0,0 @@
-G.DeclareManifest({
-name:'Add Population',
-updates:{
-	'Add Population*':'https://cdn.jsdelivr.net/gh/GothicHamsterJR/testingmod@5342e1d214f3541f2f8e475887f8fdee05d078c8/Legacytesting.js',//we're updating any mod with a name that begins with "Example mod"
-	//you could declare other mod updates here if you wanted
-}
-});
